126648ba9c2901bbf3e3627091ac96af8d41a9c2
Robin Renneke <rennekerobin@yahoo.com>
2020-08-10 14:29:46 -0500

    HTML with Bootstrap rows with CSS and Script vars

---
+++ script.js
@@ -0,0 +1,16 @@
+//variables to get the current date and time from"https://tecadmin.net/get-current-date-time-javascript/""
+var today = new Date();
+var currentDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+var currentHour = today.getHours();
+
+
+//variables for each hour
+var time9 = document.getElementById("input9")
+var time10 = document.getElementById("input10")
+var time11 = document.getElementById("input11")
+var time12 = document.getElementById("input12")
+var time1 = document.getElementById("input1")
+var time2 = document.getElementById("input2")
+var time3 = document.getElementById("input3")
+var time4 = document.getElementById("input4")
+var time5 = document.getElementById("input5")
